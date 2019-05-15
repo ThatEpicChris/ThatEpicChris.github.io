2681824110bd3813b527653e9cb60d61c45e8a00
Christopher Bravata <thechrisbravata@gmail.com>
2019-05-14 22:00:14 -0500

    Website Update 1.2

--- assets/populator/populator.js
+++ assets/populator/populator.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $.getJSON('assets/populator/population.json', function (population) {
+    $.getJSON('assets/populator/data.json', function (population) {
       // LINE 1: Variable  Name & JSON Object Pairing
       // LINE 2: HTML Pairing & Variable Pairing
-      var h1title = population.h1title;
+      /* var h1title = population.h1title;
       $( "headertitle1" ).append(h1title);
       var h2title = population.h2title;
       $( "headertitle2" ).append(h2title);
@@ -13,13 +13,13 @@ $(document).ready(function () {
       var h4title = population.h4title;
       $( "headertitle4" ).append(h4title);
       var footertxt = population.footertxt;
-      $( "footertxt" ).append(footertxt);
+      $( "footertxt" ).append(footertxt); */
 
        // LINE 1: Family Name (doc.category)
        // LINE 2: HTML Pairing & Variable Pairing (Use 'this.object' where object is the section title within the Category)
        $.each(population.works, function (works) {
-        $( "wkcard" ).append(
-          "<a href=#" + this.id +"><div class=wk-card><div class=wk-img  style=background-image:url(" + this.img + ")></div><div class=wk-title>" + this.title + "</div><div class=wk-tag>" + this.tag + "</div></div></a><div class=remodal data-remodal-id=" + this.id + "><div class=wk-title>" + this.title + "</div><br><div class=wk-img-inline  style=background-image:url(" + this.img + ")></div><br><div class=wk-content>" + this.content + "</div><br><a class=btn href=" + this.link + ">VIEW PROJECT</a></div>"
+        $( "#project-list" ).append(
+          "<div class= project ><img src=" + this.img + "  class= pj-graphic /><div class= pj-title >" + this.title + "</div><div class=pj-subtitle>" + this.subtitle + "</div><a href=" + this.link + "><div class= pj-expand ><i class= material-icons id= pj-icon >remove_red_eye</i> View Project</div></a></div>"
         );
 
     });
